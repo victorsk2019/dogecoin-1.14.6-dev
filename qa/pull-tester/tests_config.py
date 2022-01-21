68d4e28accc6423146d726eac66ad1f42b10d4ea
victorsk2019 <47955466+victorsk2019@users.noreply.github.com>
2022-01-20 18:21:56 -0800

    Initial commit

---
+++ qa/pull-tester/tests_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) 2013-2016 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+SRCDIR="/home/me/versions/fromgit/dogecoin-1.14.6-fix-scrypt-sse2"
+BUILDDIR="/home/me/versions/fromgit/dogecoin-1.14.6-fix-scrypt-sse2"
+EXEEXT=""
+
+# These will turn into comments if they were disabled when configuring.
+ENABLE_WALLET=1
+ENABLE_UTILS=1
+ENABLE_BITCOIND=1
+#ENABLE_ZMQ=1
